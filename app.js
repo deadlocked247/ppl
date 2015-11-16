66eee8e63027516ff9e79aeeefd8554a9c2ac619
Burak Aslan <Burak_Aslan@intuit.com>
2015-11-16 14:13:40 -0800

    asd

--- app.js
+++ app.js
@@ -2,9 +2,37 @@ var express  = require('express');
 var app      = express(); 
 var port  	 = process.env.PORT || 4000;
 var bodyParser = require('body-parser'); 
-app.use(express.static(__dirname + '/public')); 	
+var rq = require('request');
+
+app.use(express.static(__dirname + '/public')); 
+
+app.get('/login', function (req, res) {
+	res.sendFile(__dirname + '/public/login.html');
+});
+
+
 app.use(bodyParser.json());
 
+
+
+app.post('/api/login', function(req, res) {
+	rq({
+		url: 'http://45.55.1.41:8080/login',
+		method: 'POST',
+		form:req.body,
+		header: {
+			'Content-type': 'application/x-www-form-urlencoded'
+			}
+		},
+		function(error, response, body) {
+			res.set(response.headers);
+			var cookie = response.headers['set-cookie'][0].split(";");
+			cookie = cookie[0].replace('connect.sid=', '');
+			res.cookie('connect.sid',  cookie, { maxAge: 900000, httpOnly: false});
+			console.log(body);
+			res.send(body);
+		});
+})
 
 app.listen(port);
 console.log("PPL listening on port " + port);
--- public/components/app.js
+++ public/components/app.js
@@ -1,6 +1,50 @@
-app = angular.module('ppl', []);
 
+(function () {
+	'use strict'; 
 
-app.controller('appController', ['$scope', function($scope) {
-	$scope.menu = "name";
-}]);
+	angular.module('ppl', ['ngRoute', 'ngCookies'])
+	.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+		$locationProvider.html5Mode(true);
+		$routeProvider
+		.when('/', {
+		    templateUrl: '../workout.html',
+		    controller: 'workoutController'
+		})
+		.when('/login', {
+		    templateUrl: '../login.html',
+		    controller: 'loginController'
+		});
+	}])
+	.service('dataService', function($q, $http){
+		return {
+			login : function(body) {
+				return $http({
+					url:'/api/login',
+					method: "POST",
+					data: body
+				});
+			}
+		};
+	})
+	.controller('loginController', function($scope, dataService, $cookies, $location, $rootScope) {
+		$scope.submit = function() {
+			var body = 
+			{
+				email: $scope.user,
+				password: $scope.pass
+			}
+			dataService.login(body)
+			.then(function (payload) {
+				$rootScope.user = angular.copy(payload.data);
+				console.log($rootScope.user);
+				$location.absUrl = '/';
+			});
+		}
+	})
+	.controller('workoutController', function($scope, $rootScope) {
+		$scope.menu = false;
+
+	});
+	
+
+})();
