65092ee28ccb36deb343841f6f3cec690d064eb1
Burak Aslan <Burak_Aslan@intuit.com>
2015-11-16 16:14:21 -0800

    fixes

--- app.js
+++ app.js
@@ -6,22 +6,30 @@ var rq = require('request');
 
 app.use(express.static(__dirname + '/public')); 
 
-app.get('/login', function (req, res) {
-	res.sendFile(__dirname + '/public/login.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/routines', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/login', function (req, res) {
+	res.sendFile(__dirname + '/public/index.html');
+});
 
 app.use(bodyParser.json());
 
 
 
 app.post('/api/login', function(req, res) {
+
 	rq({
 		url: 'http://45.55.1.41:8080/login',
 		method: 'POST',
 		form:req.body,
 		header: {
-			'Content-type': 'application/x-www-form-urlencoded'
+			'Content-type': 'application/json'
 			}
 		},
 		function(error, response, body) {
@@ -30,7 +38,50 @@ app.post('/api/login', function(req, res) {
 			cookie = cookie[0].replace('connect.sid=', '');
 			res.cookie('connect.sid',  cookie, { maxAge: 900000, httpOnly: false});
 			console.log(body);
-			res.send(body);
+			if(error) {
+				res.send(new Error(error));
+			}
+			else {
+				res.send(body);
+			}
+		});
+})
+
+
+app.get('/api/user/:id', function(req, res) {
+	console.log(req.headers);
+	rq({
+		url: 'http://45.55.1.41:8080/api/users/id/' + req.params.id,
+		method: 'GET',
+		header: req.headers
+	},
+		function(error, response, body) {
+			console.log(body);
+			if(error) {
+				res.send(new Error(error));
+			}
+			else {
+				res.send(body);
+			}
+		});
+})
+
+
+app.get('/api/routines', function(req, res) {
+	console.log(req.headers);
+	rq({
+		url: 'http://45.55.1.41:8080/api/routines',
+		method: 'GET',
+		header: req.headers
+	},
+		function(error, response, body) {
+			console.log(body);
+			if(error) {
+				res.send(new Error(error));
+			}
+			else {
+				res.send(body);
+			}
 		});
 })
 
--- public/components/app.js
+++ public/components/app.js
@@ -10,10 +10,30 @@
 		    templateUrl: '../workout.html',
 		    controller: 'workoutController'
 		})
+		.when('/routines', {
+		    templateUrl: '../routines.html',
+		    controller: 'routineController'
+		})
 		.when('/login', {
 		    templateUrl: '../login.html',
 		    controller: 'loginController'
 		});
+	}])
+	.run(['$rootScope', '$cookies', '$location', 'dataService', function ($rootScope, $cookies, $location, dataService) {
+		if(!$cookies.get('connect.sid')) {
+			$rootScope.loggedIn = false;
+			$rootScope.menu = false;
+			$location.path('/login');
+		}
+		else if (!$rootScope.user){
+			$rootScope.loggedIn = true;
+			$rootScope.loading = true;
+			dataService.userInfo(window.localStorage.getItem("userID"))
+			.then(function (payload) {
+				$rootScope.user = payload.data;
+				$rootScope.loading = false;
+			});
+		}
 	}])
 	.service('dataService', function($q, $http){
 		return {
@@ -23,11 +43,31 @@
 					method: "POST",
 					data: body
 				});
+			},
+
+			routines : function() {
+				return $http({
+					url:'/api/routines',
+					method: "GET"
+				});
+			},
+
+			userInfo : function(id) {
+				return $http({
+					url:'/api/user/' + id,
+					method: "GET"
+				});
 			}
 		};
 	})
 	.controller('loginController', function($scope, dataService, $cookies, $location, $rootScope) {
+		$rootScope.loggedIn = false;
+		if($cookies.get('connect.sid')) {
+			$rootScope.loggedIn = true;
+			$location.path('/');
+		}
 		$scope.submit = function() {
+			console.log("ASD");
 			var body = 
 			{
 				email: $scope.user,
@@ -36,7 +76,7 @@
 			dataService.login(body)
 			.then(function (payload) {
 				$rootScope.user = angular.copy(payload.data);
-				console.log($rootScope.user);
+				$rootScope.loggedIn = true;
 				window.localStorage.setItem("userID", payload.data._id);
 				$location.path('/');
 			});
@@ -45,16 +85,33 @@
 	.controller('menuController', function($scope, $cookies, $rootScope, $location) {
 		$rootScope.menu = false;
 
+		$scope.active = '';
+
+
+		$scope.goTo = function(where) {
+			$rootScope.menu = false;
+			$scope.active = where;
+			$location.path('/' + where);
+		}
+
 		$scope.logout = function() {
 			$cookies.remove('connect.sid');
 			$rootScope.menu = false;
+			$rootScope.loggedIn = false;
 			$location.path('/login');
 		}
 	})
-	.controller('workoutController', function($scope, $rootScope, $cookies, $location) {
-		if(!$cookies.get('connect.sid')) {
-			$location.path('/login');
-		}
+	.controller('routineController', function ($scope, $rootScope, dataService, $location, $cookies) {
+		$scope.loading = true;
+
+		dataService.routines()
+		.then(function (payload) {
+			$scope.loading = false;
+			$scope.routines = payload.data;
+		})
+	})
+	.controller('workoutController', function ($scope, $rootScope, $cookies, $location, dataService) {
+		
 
 	});
 	
